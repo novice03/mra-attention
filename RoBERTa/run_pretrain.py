47c19d70df0f0b4a6278abd556d26e1d6dc6bd94
novice <pranavpulijala@gmail.com>
2022-11-24 15:37:13 -0600

    Update run_pretrain.py

--- RoBERTa/run_pretrain.py
+++ RoBERTa/run_pretrain.py
@@ -16,7 +16,7 @@ seed = 0
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.benchmark = False
 random.seed(seed)
 os.environ['PYTHONHASHSEED'] = str(seed)
 np.random.seed(seed)
