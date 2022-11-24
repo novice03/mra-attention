ad2c6cd3b5ec9d814b5ad7980a9a801d2af7a8ab
novice <pranavpulijala@gmail.com>
2022-11-24 17:04:32 -0600

    Update mra2.py

--- RoBERTa/encoders/mra2.py
+++ RoBERTa/encoders/mra2.py
@@ -69,7 +69,7 @@ class Attention(nn.Module):
         K = self.split_heads(self.W_k(X))
         V = self.split_heads(self.W_v(X))
 
-        print('query', Q)
+        #print('query', Q)
         print(self.num_block, self.approx_mode, self.initial_prior_first_n_blocks, self.initial_prior_diagonal_n_blocks)
 
         with torch.cuda.amp.autocast(enabled = False):
@@ -79,7 +79,6 @@ class Attention(nn.Module):
                 initial_prior_first_n_blocks = self.initial_prior_first_n_blocks,
                 initial_prior_diagonal_n_blocks = self.initial_prior_diagonal_n_blocks
             )
-            print('attn out', attn_out)
 
         attn_out = self.combine_heads(attn_out)
 
