4588a367cd9870ac799bbacfa616d3c1b9b70ad8
novice <pranavpulijala@gmail.com>
2022-11-24 17:00:10 -0600

    Update attention.py

--- RoBERTa/encoders/mra2_kernel/attention.py
+++ RoBERTa/encoders/mra2_kernel/attention.py
@@ -102,6 +102,9 @@ def mra2_attention(
         low_resolution_logit_normalized = low_resolution_logit - low_resolution_logit_row_max
         indices, high_resolution_mask = get_block_idxes(low_resolution_logit_normalized, num_blocks, approx_mode, initial_prior_first_n_blocks, initial_prior_diagonal_n_blocks)
 
+    print('low_resolution_logit_normalized', low_resolution_logit_normalized)
+    print('indices', indices)
+
     high_resolution_logit = SampledDenseMM.operator_call(Q, K, indices, block_size = block_size) / math.sqrt(head_dim)
     max_vals, max_vals_scatter = sparse_max(high_resolution_logit, indices, num_block_per_row, num_block_per_row)
     high_resolution_logit = high_resolution_logit - max_vals_scatter
